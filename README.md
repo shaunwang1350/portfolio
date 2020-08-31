1c10a51c7dde88e265099b691781ddd4a29a60ce
Owangatang <shaunwangg@gmail.com>
2020-08-31 18:20:02 -0400

    initial design

--- README.md
+++
@@ -1,5 +0,0 @@
-# Portfolio
-
-### Version:
-
-* This is a very preliminary HTML/CSS structure of my portfolio. It is essentially ported information from my CV.
---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+const imgContent = document.querySelectorAll('.img-content-hover');
+
+function showImgContent(e) {
+    for (var i = 0; i < imgContent.length; i++) {
+        imgContent[i].style.left = e.pageX + 'px';
+        imgContent[i].style.top = e.pageY + 'px';
+    }
+};
+
+document.addEventListener('mousemove', showImgContent);
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
